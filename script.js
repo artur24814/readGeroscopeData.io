38a5f1e452bc3ad890832216adaae7f2b71fe4db
artur24814 <artur24814@gmail.com>
2023-03-29 16:45:07 +0200

    cleaned the code

--- script.js
+++ script.js
@@ -69,60 +69,7 @@ if (typeof DeviceOrientationEvent !== 'undefined' && typeof DeviceOrientationEve
     
 }
 
-// surface
-
-// Get the canvas element from the DOM
-const canvas = document.getElementById('scene');
-
-// Get the canvas dimensions
-let width = canvas.width; // Width of the scene
-let height = canvas.height; // Height of the scene
-
-// Store the 2D context
-const ctx = canvas.getContext('2d');
-
-// Function called right after user resized its screen
-function onResize () {
-  // We need to define the dimensions of the canvas to our canvas element
-  // Javascript doesn't know the computed dimensions from CSS so we need to do it manually
-  width = canvas.offsetWidth;
-  height = canvas.offsetHeight;
-  
-  // If the screen device has a pixel ratio over 1
-  // We render the canvas twice bigger to make it sharper (e.g. Retina iPhone)
-  if (window.devicePixelRatio > 1) {
-    canvas.width = canvas.clientWidth * 2;
-    canvas.height = canvas.clientHeight * 2;
-    ctx.scale(2, 2);
-  } else {
-    canvas.width = width;
-    canvas.height = height;
-  }
-}
-
-// Listen to resize events
-window.addEventListener('resize', onResize);
-// Make sure the canvas size is perfect
-onResize();
-
 function renderSurface (alpha, gama, beta) {
     let scene_body = document.querySelector('.scene-body')
     scene_body.style.transform = 'rotateZ(' + alpha + 'deg) rotateY(' + gama + 'deg) rotateX(' + beta + 'deg)';
 }
-
-function drawCanvas() {
-    ctx.fillStyle = "gray";
-    ctx.fillRect(20, 0, height, width);
-    ctx.beginPath();
-    ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
-    ctx.moveTo(110, 75);
-    ctx.arc(75, 75, 35, 0, Math.PI, false); // Mouth (clockwise)
-    ctx.moveTo(65, 65);
-    ctx.arc(60, 65, 5, 0, Math.PI * 2, true); // Left eye
-    ctx.moveTo(95, 65);
-    ctx.arc(90, 65, 5, 0, Math.PI * 2, true); // Right eye
-    ctx.stroke();
-    
-}
-
-drawCanvas()
